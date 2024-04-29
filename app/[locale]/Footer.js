fba937866741b8b1bf1cae9f84b643990f09a9af
aleksandrmaas <55091655+aleksandrmaas@users.noreply.github.com>
2024-04-29 14:20:12 +0300

    Fixed footer links

--- app/[locale]/Footer.js
+++ app/[locale]/Footer.js
@@ -9,9 +9,9 @@ const FooterComponent = () => {
     <Footer container>
       <Footer.Copyright by="Sunrise AI" year={2024} />
       <Footer.LinkGroup>
-        <Footer.Link href="/about">{t("About")}</Footer.Link>
+        <Footer.Link href={t("about_us_url")}>{t("About")}</Footer.Link>
         <Footer.Link href="/pricing">{t("Pricing")}</Footer.Link>
-        <Footer.Link href="/blog">{t("Blog")}</Footer.Link>
+        <Footer.Link href={t("blog_url")}>{t("Blog")}</Footer.Link>
         <Footer.Link href="/contact">{t("Contact")}</Footer.Link>
       </Footer.LinkGroup>
     </Footer>
--- app/[locale]/about/about.js
+++ app/[locale]/about/about.js
@@ -18,21 +18,21 @@ const About = () => {
           <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-grey-900 md:text-4xl lg:text-5xl dark:text-white">
             {t("Title")}
           </h1>
-          <h2 class="text-4xl font-extrabold dark:text-white">
+          <h2 class="mt-6 text-4xl font-extrabold dark:text-white">
             {t("Who_are_we")}
           </h2>
           <p>{t("block_1")}</p>
           <p>{t("block_2")}</p>
           <p>{t("block_3")}</p>
           <p>{t("block_4")}</p>
-          <h2 class="text-4xl font-extrabold dark:text-white">
+          <h2 class="mt-6 text-4xl font-extrabold dark:text-white">
             {t("block_5")}
           </h2>
           <p>{t("block_6")}</p>
-          <h3 class="text-3xl font-extrabold dark:text-white">
+          <h3 class="mt-6 text-3xl font-extrabold dark:text-white">
             {t("block_7")}
           </h3>
-          <ul class="max-w-md space-y-1 list-disc list-inside dark:text-gray-400">
+          <ul class="space-y-1 list-disc list-inside dark:text-gray-400">
             <li>{t("block_8")}</li>
             <li>{t("block_9")}</li>
             <li>{t("block_10")}</li>
